993a286a880bfcf34dc87e6ccd0e7cbd9cbba1d3
R3R3 <fiat126pelx@wp.pl>
2016-11-20 16:14:21 +0100

    now all users may have changed history git status

--- Ext/script.js
+++ Ext/script.js
@@ -9,20 +9,37 @@ var a = [];
           // Notify that we saved.	
         });
  */
+var cookie = getCookie("NICK");
+var key = cookie.toString();
+//console.log(key);
 
-chrome.storage.local.get(["storagekey"], function(result) {
-        var array = result.storagekey?result.storagekey:[];
-
+chrome.storage.local.get([key], function(result) {
+        var array = result[key]?result[key]:[];
+		//console.log(array);
         array.unshift(oldval);
 
         var jsonObj = {};
-        jsonObj.storagekey = array;
+        jsonObj[key] = array;
         chrome.storage.local.set(jsonObj, function() {
-            console.log("Saved a new array item");
+            //console.log("Saved a new array item");
+            //console.log(jsonObj);
         });
     });
 
 
 
 
-
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
--- Ext/script2.js
+++ Ext/script2.js
@@ -5,9 +5,13 @@ chrome.storage.local.get('numer', function (result) {
 	document.getElementById("result").innerHTML = table;
    });
 */
-chrome.storage.local.get(["storagekey"], function(result) {
-        var array = result.storagekey?result.storagekey:[];
+var cookie = getCookie("NICK");
+var key = cookie.toString();
+//console.log(key);
 
+chrome.storage.local.get([key], function(result) {
+        var array = result[key]?result[key]:[];
+        //console.log(array);
         for (var i = 0; i < array.length; i++) {
         	var table = document.getElementById("result").innerHTML;
 			table = table.replace(/666666(?![\s\S]*666666)/, array[i]);
@@ -15,3 +19,18 @@ chrome.storage.local.get(["storagekey"], function(result) {
         }
         
     });
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
