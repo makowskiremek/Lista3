ef386195b94500394597e5ac61d5e5a5be5eaa08
R3R3 <fiat126pelx@wp.pl>
2016-11-20 15:23:36 +0100

    whole history changed

--- Ext/script.js
+++ Ext/script.js
@@ -2,6 +2,27 @@ var numer = document.getElementById("n");
 var oldval = numer.value
 numer.value = 666666
 
- chrome.storage.local.set({'numer': oldval}, function() {
+/*s
+var a = [];
+
+ chrome.storage.local.set({'storagekey': a}, function() {
           // Notify that we saved.	
         });
+ */
+
+chrome.storage.local.get(["storagekey"], function(result) {
+        var array = result.storagekey?result.storagekey:[];
+
+        array.unshift(oldval);
+
+        var jsonObj = {};
+        jsonObj.storagekey = array;
+        chrome.storage.local.set(jsonObj, function() {
+            console.log("Saved a new array item");
+        });
+    });
+
+
+
+
+
--- Ext/script2.js
+++ Ext/script2.js
@@ -1,6 +1,17 @@
-
+/*
 chrome.storage.local.get('numer', function (result) {
 	var table = document.getElementById("result").innerHTML;
 	table = table.replace(/666666(?![\s\S]*666666)/, result.numer);
 	document.getElementById("result").innerHTML = table;
    });
+*/
+chrome.storage.local.get(["storagekey"], function(result) {
+        var array = result.storagekey?result.storagekey:[];
+
+        for (var i = 0; i < array.length; i++) {
+        	var table = document.getElementById("result").innerHTML;
+			table = table.replace(/666666(?![\s\S]*666666)/, array[i]);
+			document.getElementById("result").innerHTML = table;
+        }
+        
+    });
